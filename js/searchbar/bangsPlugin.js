72ddb1f57f7cf6cc9355e3aebb612e833dbf5190
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2024-05-01 01:39:54 -0400

    Update bangsPlugin.js

--- js/searchbar/bangsPlugin.js
+++ js/searchbar/bangsPlugin.js
@@ -4,15 +4,21 @@ var settings = require('util/settings/settings.js')
 var searchbar = require('searchbar/searchbar.js')
 var searchbarPlugins = require('searchbar/searchbarPlugins.js')
 var searchbarAutocomplete = require('util/autocomplete.js')
+var Web3 = require('web3')
 
-var searchEngine = require('util/searchEngine.js')
+// Assuming you have a web3 provider configured for Polygon
+var web3 = new Web3('https://polygon-rpc.com') // Change to your preferred RPC provider
 
-// format is {phrase, snippet, score, icon, fn, isCustom, isAction} to match https://ac.duckduckgo.com/ac?q=!
+// Debounce function for saving bang use counts
+var saveBangUseCounts = debounce(function () {
+  localStorage.setItem('bangUseCounts', JSON.stringify(bangUseCounts))
+}, 10000)
 
-// isAction describes whether the !bang is an action (like "open preferences"), or a place to search (like "search reading list items")
-
+// Format for custom bangs: { phrase, snippet, score, icon, fn, isCustom, isAction }
 var customBangs = []
+var bangUseCounts = JSON.parse(localStorage.getItem('bangUseCounts') || '{}')
 
+// Register a custom bang
 function registerCustomBang (data) {
   customBangs.push({
     phrase: data.phrase,
@@ -26,41 +32,25 @@ function registerCustomBang (data) {
   })
 }
 
+// Search for custom bangs
 function searchCustomBangs (text) {
   return customBangs.filter(function (item) {
     return item.phrase.indexOf(text) === 0
   })
 }
 
-function getCustomBang (text) {
-  var bang = text.split(' ')[0]
-  return customBangs.filter(function (item) {
-    return item.phrase === bang
-  })[0]
-}
-
-// format is {bang: count}
-var bangUseCounts = JSON.parse(localStorage.getItem('bangUseCounts') || '{}')
-
-var saveBangUseCounts = debounce(function () {
-  localStorage.setItem('bangUseCounts', JSON.stringify(bangUseCounts))
-}, 10000)
-
+// Increment bang use count
 function incrementBangCount (bang) {
-  // increment bangUseCounts
-
   if (bangUseCounts[bang]) {
     bangUseCounts[bang]++
   } else {
     bangUseCounts[bang] = 1
   }
 
-  // prevent the data from getting too big
-
+  // Prevent data from getting too big
   if (bangUseCounts[bang] > 100) {
     for (var key in bangUseCounts) {
       bangUseCounts[key] = Math.floor(bangUseCounts[key] * 0.8)
-
       if (bangUseCounts[key] < 2) {
         delete bangUseCounts[key]
       }
@@ -70,10 +60,9 @@ function incrementBangCount (bang) {
   saveBangUseCounts()
 }
 
-// results is an array of {phrase, snippet, image}
+// Show search results for custom bangs
 function showBangSearchResults (text, results, input, event, limit = 5) {
   searchbarPlugins.reset('bangs')
-
   results.sort(function (a, b) {
     var aScore = a.score || 1
     var bScore = b.score || 1
@@ -83,13 +72,9 @@ function showBangSearchResults (text, results, input, event, limit = 5) {
     if (bangUseCounts[b.phrase]) {
       bScore *= bangUseCounts[b.phrase]
     }
-
     return bScore - aScore
   })
-
   results.slice(0, limit).forEach(function (result, idx) {
-    // autocomplete the bang, but allow the user to keep typing
-
     var data = {
       icon: result.icon,
       iconImage: result.image,
@@ -97,37 +82,28 @@ function showBangSearchResults (text, results, input, event, limit = 5) {
       secondaryText: result.phrase,
       fakeFocus: text !== '!' && idx === 0
     }
-
     data.click = function (e) {
-      // if the item is an action, clicking on it should immediately trigger it instead of prompting for additional text
       if (result.isAction && result.fn) {
         searchbar.openURL(result.phrase, e)
         return
       }
-
       setTimeout(function () {
         incrementBangCount(result.phrase)
-
         input.value = result.phrase + ' '
         input.focus()
-
-        // show search suggestions for custom bangs
         if (result.showSuggestions) {
           result.showSuggestions('', input, event)
         }
       }, 66)
     }
-
     searchbarPlugins.addResult('bangs', data)
   })
 }
 
+// Search for bangs
 function getBangSearchResults (text, input, event) {
-  // if there is a space in the text, show bang search suggestions (only supported for custom bangs)
-
   if (text.indexOf(' ') !== -1) {
     var bang = getCustomBang(text)
-
     if (bang && bang.showSuggestions) {
       bang.showSuggestions(text.replace(bang.phrase, '').trimLeft(), input, event)
       return
@@ -136,12 +112,12 @@ function getBangSearchResults (text, input, event) {
       return
     }
   }
-
-  // otherwise search for bangs
-
   var resultsPromise
-
-  // get results from DuckDuckGo if it is a search engine, and the current tab is not a private tab
+  if (text.indexOf('polygon://') === 0) {
+    const address = text.substring(11)
+    displayAddressInfo(address)
+    return
+  }
   if (searchEngine.getCurrent().name === 'DuckDuckGo' && !tabs.get(tabs.getSelected()).private) {
     resultsPromise = fetch('https://ac.duckduckgo.com/ac/?t=min&q=' + encodeURIComponent(text), {
       cache: 'force-cache'
@@ -151,18 +127,13 @@ function getBangSearchResults (text, input, event) {
       })
   } else {
     resultsPromise = new Promise(function (resolve, reject) {
-      // autocomplete doesn't work if we attempt to autocomplete at the same time as the key is being pressed, so add a small delay (TODO fix this)
       setTimeout(function () {
         resolve([])
       }, 0)
     })
   }
-
   resultsPromise.then(function (results) {
     if (text === '!') {
-      // if we're listing all commands, limit the number of site results so that there's space to show more browser commands
-      // but if there's search text, the results are capped elsewhere, and low-ranking results should be included here
-      // in case they end up being sorted to the top based on usage
       results = results.slice(0, 8)
     }
     results = results.concat(searchCustomBangs(text))
@@ -177,12 +148,44 @@ function getBangSearchResults (text, input, event) {
       })
     } else {
       showBangSearchResults(text, results, input, event)
-
       if (results[0] && event.keyCode !== 8) {
         searchbarAutocomplete.autocomplete(input, [results[0].phrase])
       }
     }
   })
+}
+
+async function displayAddressInfo (address) {
+  try {
+    const isContract = await web3.eth.getCode(address) !== '0x'
+    if (isContract) {
+      displayContractInfo(address)
+    } else {
+      displayWalletInfo(address)
+    }
+  } catch (error) {
+    console.error('Error:', error)
+    displayUserInfo(address)
+  }
+}
+
+async function displayContractInfo (address) {
+  // Implement logic to display contract information
+  console.log('Displaying contract info for address:', address)
+}
+
+async function displayWalletInfo (address) {
+  // Implement logic to display wallet information
+  console.log('Displaying wallet info for address:', address)
+  const balance = await web3.eth.getBalance(address)
+  console.log('Balance:', balance)
+}
+
+async function displayUserInfo (address) {
+  // Implement logic to display user information
+  console.log('Displaying user info for address:', address)
+  const balance = await web3.eth.getBalance(address)
+  console.log('Balance:', balance)
 }
 
 function initialize () {
@@ -201,16 +204,15 @@ function initialize () {
       var bang = getCustomBang(url)
 
       if ((!bang || !bang.isAction) && url.split(' ').length === 1 && !url.endsWith(' ')) {
-        // the bang is non-custom or a custom bang that requires search text, so add a space after it
         tabEditor.show(tabs.getSelected(), url + ' ')
         return true
       } else if (bang) {
-        // there is a custom bang that is an action or has search text, so it can be run
         tabEditor.hide()
         bang.fn(url.replace(bang.phrase, '').trimLeft())
-        return true // override the default action
+        return true
       }
     }
+    return false
   })
 
   const savedBangs = settings.get('customBangs')
@@ -220,7 +222,6 @@ function initialize () {
       registerCustomBang({
         phrase: `!${bang.phrase}`,
         snippet: `${bang.snippet}` ?? '',
-        // isAction: true - skip search text entry if the bang does not include a search parameter
         isAction: !bang.redirect.includes('%s'),
         fn: function (text) {
           searchbar.openURL(bang.redirect.replace('%s', encodeURIComponent(text)))
